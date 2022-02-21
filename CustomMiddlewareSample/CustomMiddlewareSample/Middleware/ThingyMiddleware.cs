abc68b291ed85afa2dfe9973f468e481ebcf0122
André Davis <CubicleJockey@users.noreply.github.com>
2022-02-21 14:49:28 -0800

    Init checkin

---
+++ CustomMiddlewareSample/CustomMiddlewareSample/Middleware/ThingyMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+namespace CustomMiddlewareSample.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ThingyMiddleware
+    {
+        private const string Thingy = nameof(Thingy);
+        private readonly RequestDelegate next;
+
+        public ThingyMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            var thingyQueryItem = httpContext.Request.Query[Thingy];
+            if (string.IsNullOrWhiteSpace(thingyQueryItem))
+            {
+                Debug.WriteLine($"{Thingy} was not present in the query string.");
+            }
+            return next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ThingyMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseThingyMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ThingyMiddleware>();
+        }
+    }
+}
